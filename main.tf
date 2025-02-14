451696aecbdae8a0ef8aa1414656d5f2e7507106
Houss-DZ <faidhousseine@gmail.com>
2025-02-14 14:59:07 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,29 +18,12 @@ data "aws_ami" "ubuntu" {
   }
 }
 
-# Création de l'instance EC2 avec NGINX et Hello World
+# Création de l'instance EC2 avec l'AMI la plus récente
 resource "aws_instance" "web" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
-  key_name      = "ma clé ssh"  # Remplace par le nom de ta clé SSH existante
+  ami           = data.aws_ami.ubuntu.id  # Utilisation de l'AMI trouvée automatiquement
+  instance_type = "t2.micro"              # Type d'instance (éligible au niveau gratuit AWS)
 
   tags = {
     Name = "YvanHousseine"
   }
-
-  user_data = <<-EOF
-              #!/bin/bash
-              apt-get update
-              apt-get install -y nginx
-              echo "Hello World from Terraform with NGINX on $(hostname)" > /var/www/html/index.html
-              systemctl start nginx
-              systemctl enable nginx
-              EOF
 }
-
-# Sortie pour afficher l'adresse publique de l'instance
-output "instance_public_ip" {
-  description = "Adresse IP publique de l'instance EC2"
-  value       = aws_instance.web.public_ip
-}
-
