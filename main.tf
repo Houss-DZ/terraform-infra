486aee051ec2bd6b4083b63f59a051bee3058829
Houss-DZ <faidhousseine@gmail.com>
2025-02-14 14:18:57 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,11 +2,29 @@ provider "aws" {
   region = "eu-central-1"  # Région AWS : Frankfurt
 }
 
+# Automatisation pour trouver la dernière AMI Ubuntu 20.04 LTS
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]  # Canonical, propriétaire des AMI Ubuntu
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# Création de l'instance EC2 avec l'AMI la plus récente
 resource "aws_instance" "web" {
-  ami           = "ami-08c40ec9ead489470"  # Ubuntu 20.04 LTS AMI pour Frankfurt (eu-central-1)
-  instance_type = "t2.micro"               # Instance de type t2.micro (éligible au niveau gratuit AWS)
+  ami           = data.aws_ami.ubuntu.id  # Utilisation de l'AMI trouvée automatiquement
+  instance_type = "t2.micro"              # Type d'instance (éligible au niveau gratuit AWS)
 
   tags = {
     Name = "YvanHousseine"
   }
 }
+
