39ba157882d9b765d6839e2d6e11d7fb397e3d03
Houss-DZ <faidhousseine@gmail.com>
2025-02-14 15:29:28 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,21 +2,33 @@ provider "aws" {
   region = "eu-central-1"  # Région AWS : Frankfurt
 }
 
+# Génération d'une clé privée RSA
 resource "tls_private_key" "my_key" {
   algorithm = "RSA"
   rsa_bits  = 2048
 }
 
+# Création du Key Pair AWS avec la clé publique
 resource "aws_key_pair" "my_key_pair" {
   key_name   = "yvanhousseine-key"
   public_key = tls_private_key.my_key.public_key_openssh
 }
 
+# Enregistrement de la clé privée dans un fichier local
 resource "local_file" "private_key" {
-  filename   = "./yvanhousseine-key.pem"  # La clé privée sera stockée dans le dossier actuel
-  content    = tls_private_key.my_key.private_key_pem
-  file_permission = "0600"  # Permission sécurisée pour la clé
+  filename        = "./yvanhousseine-key.pem"  # Clé privée enregistrée localement
+  content         = tls_private_key.my_key.private_key_pem
+  file_permission = "0600"
 }
+
+# Envoi de la clé privée vers un bucket S3
+resource "aws_s3_bucket_object" "private_key_to_s3" {
+  bucket  = "hocine-ons"             # Nom du bucket S3
+  key     = "ssh/yvanhousseine-key.pem"  # Chemin dans le bucket
+  source  = "./yvanhousseine-key.pem"    # Fichier local généré précédemment
+  acl     = "private"               # Rend la clé privée accessible uniquement à l’utilisateur AWS
+}
+
 
 # Automatisation pour trouver la dernière AMI Ubuntu 20.04 LTS
 data "aws_ami" "ubuntu" {
