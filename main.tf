31a507368368b0a02d717336730e112aa09808f5
Houss-DZ <faidhousseine@gmail.com>
2025-02-14 15:18:16 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,22 @@ provider "aws" {
   region = "eu-central-1"  # Région AWS : Frankfurt
 }
 
+resource "tls_private_key" "my_key" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+resource "aws_key_pair" "my_key_pair" {
+  key_name   = "yvanhousseine-key"
+  public_key = tls_private_key.my_key.public_key_openssh
+}
+
+resource "local_file" "private_key" {
+  filename   = "./yvanhousseine-key.pem"  # La clé privée sera stockée dans le dossier actuel
+  content    = tls_private_key.my_key.private_key_pem
+  file_permission = "0600"  # Permission sécurisée pour la clé
+}
+
 # Automatisation pour trouver la dernière AMI Ubuntu 20.04 LTS
 data "aws_ami" "ubuntu" {
   most_recent = true
